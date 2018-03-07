aa04657eedbb601a16c82a8a57ab306175a76c9d
Saif Tase <saif.tase@sulzer-us.com>
2018-03-07 12:18:09 -0500

    Sorting using lambdas

--- src/com/teamtreehouse/lambdas/Main.java
+++ src/com/teamtreehouse/lambdas/Main.java
@@ -20,7 +20,25 @@ public class Main {
         }
     }
 
+    public static void usingLambdasInLongForm(){
+        List<Book> books = Books.all();
+        Collections.sort(books, (Book b1, Book b2) -> {
+            return b1.getTitle().compareTo(b2.getTitle());
+        });
+
+        for(Book book: books){
+            System.out.println(book);
+        }
+    }
+
+    public static void usingLambdasInShortForm(){
+        List<Book> books = Books.all();
+        Collections.sort(books, (b1, b2) -> b1.getTitle().compareTo(b2.getTitle()));
+        books.forEach(book-> System.out.println(book));
+    }
+
+
     public static void main(String[] args) {
-	    usingAnonymousInlineClass();
+	    usingLambdasInShortForm();
     }
 }
